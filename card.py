08d65bc5c8518fdf86b6b7dc5a8ca5d988e0f44e
Haider Abdullah <124008394+haiderabdullah135@users.noreply.github.com>
2023-06-14 13:32:28 -0400

    Updated card.py
    line 4 was too long, shortened it so it works with action request

--- card.py
+++ card.py
@@ -1,7 +1,8 @@
 class Card:
     def __init__(self,value,suit):
         self.cost = value
-        self.value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'][value-1]
+        self.value=['A','2','3','4','5','6','7','8','9','10','J','Q','K'][value-1]
+        
         self.suit = '♥♦♣♠'[suit]
         
     def show(self):
